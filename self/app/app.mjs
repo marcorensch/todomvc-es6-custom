9af5a24cc96559cd2b17a2be2d476518b24fe6b7
Marco <marco.rensch@area59.ch>
2022-05-07 11:23:23 +0200

    Ende 7.5

--- self/app/app.mjs
+++ self/app/app.mjs
@@ -1,23 +1,30 @@
-const input = document.querySelector('.new-todo')
-const list = document.querySelector('.todo-list')
-const errorDiv = document.createElement('div')
-errorDiv.classList.add('error')
-
-class View{
-
+class View {
     constructor(store) {
         this.store = store
-        list.addEventListener('click', (e)=>{
-            if(e.target.classList.contains('destroy')){
+        this.input = document.querySelector('.new-todo')
+        this.list = document.querySelector('.todo-list')
+        this.errorDiv = document.createElement('div')
+        this.errorDiv.classList.add('error')
+
+        this.list.addEventListener('click', (e) => {
+            if (e.target.classList.contains('destroy')) {
                 const id = Number(e.target.closest('[data-id]').dataset.id)
                 console.log(id)
-                this.store.remove(id)
-                e.target.closest('li').remove()
+                if (this.onRemoveItem(id)) {
+                    e.target.closest('li').remove()
+                }
+            }
+        })
+
+        this.input.addEventListener('keyup', (ev) => {
+            if (ev.code === 'Enter') {
+                const text = this.input.value
+                this.onAddItem(text)
             }
         })
     }
 
-    createListItem(todo){
+    createListItem(todo) {
         const li = document.createElement('li')
         li.innerHTML = `<div class="view" data-id="${todo.id}">
       <input class="toggle" />
@@ -26,9 +33,38 @@ class View{
       </div>`
         return li
     }
+
+    renderTodos(todos) {
+        for (const todo of todos) {
+            const li = view.createListItem(todo)
+            this.list.appendChild(li)
+        }
+    }
+
+    setOnAddItemCallback(fn) {
+        this.onAddItem = fn
+    }
+
+    setOnRemoveItemCallback(fn) {
+        this.onRemoveItem = fn
+    }
+
+    renderError(text) {
+        this.errorDiv.innerText = text
+        this.input.parentNode.appendChild(this.errorDiv)
+    }
+
+    renderItem(item) {
+        this.errorDiv.remove()
+        const li = this.createListItem(item)
+        this.list.appendChild(li)
+        this.input.value = ''
+    }
 }
+
 class Store {
-    todos
+    #todos
+
     constructor() {
         this.todos = this.loadAllToDos()
     }
@@ -50,33 +86,45 @@ class Store {
     }
 
     remove(id) {
-        const itemIndex = todos.findIndex(i => i.id === id)
+        let lengthBefore = this.todos.length
+        const itemIndex = this.todos.findIndex(i => i.id === id)
         this.todos.splice(itemIndex, 1)
         this.storeToDos()
+        return this.todos.length < lengthBefore
+    }
+}
+
+class Controller {
+    constructor(store, view) {
+        this.store = store
+        this.view = view
+        const todos = this.store.loadAllToDos();
+        this.view.renderTodos(todos)
+        this.view.setOnAddItemCallback((...a)=>{this.onAddItem(...a)})
+        this.view.setOnRemoveItemCallback((...a) => this.onRemoveItem(...a))
+    }
+
+    onAddItem(text) {
+        if (text.length === 0) {
+            this.view.renderError("Text darf nicht leer sein!")
+        } else {
+            const id = this.store.add({text})
+            this.view.renderItem({id, text})
+        }
+    }
+
+    onRemoveItem(id) {
+        if (!this.store.remove(id)) {
+            this.view.renderError(`Element mit der ID ${id} konnte nicht gelöscht werden`)
+            return false
+        } else {
+            // this.view.renderError(`Element mit der ID ${id} gelöscht!`)
+            return true
+        }
     }
 }
 
 const store = new Store()
 const view = new View(store)
-const todos = store.loadAllToDos();
+const ctrl = new Controller(store, view)
 
-for (const todo of todos){
-    const li = view.createListItem(todo)
-    list.appendChild(li)
-}
-
-input.addEventListener('keyup', (ev) => {
-    if(ev.code === 'Enter'){
-        const text = input.value
-        if(text.length === 0){
-            errorDiv.innerText = "Text darf nicht leer sein!"
-            input.parentNode.appendChild(errorDiv)
-        }else{
-            errorDiv.remove()
-            const id = store.add({ text })
-            const li = view.createListItem({id,text})
-            list.appendChild(li)
-            input.value = ''
-        }
-    }
-})
